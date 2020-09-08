e8e2615631323fe09a3609b53c61c59177beb8c3
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2020-09-08 18:48:14 -0300

    CameraFollow

---
+++ SetembroJam/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Vector2 maxXandY;
+    [SerializeField] Vector2 minXandY;
+    private Transform player;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    private void LateUpdate()
+    {
+        float x = Mathf.Clamp(player.position.x, minXandY.x, maxXandY.x);
+        float y = Mathf.Clamp(player.position.y, minXandY.y, maxXandY.y);
+        this.transform.position = new Vector3(x,y, this.transform.position.z);
+    }
+}

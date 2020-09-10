f4a9b2630ff145829e034065e75fdcf9b1b0633c
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2020-09-10 16:48:10 -0300

    Enemy Health

---
+++ SetembroJam/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 3;
+    [SerializeField] private HealthBar hb;
+    [SerializeField] private bool canTakeDamage = false;
+    private int healthAtual;
+
+    private void Awake()
+    {
+        healthAtual = maxHealth;
+        hb.SetMaxHealth(maxHealth);
+    }
+    public void TakeDamage(int damage)
+    {
+        if (canTakeDamage)
+        {
+            healthAtual -= damage;
+        }
+    }
+    public void CanDamage(bool can)
+    {
+        canTakeDamage = can;
+    }
+    private void Update()
+    {
+        hb.SetHealth(healthAtual);
+    }
+}

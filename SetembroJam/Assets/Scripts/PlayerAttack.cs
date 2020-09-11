7c303dc122b506616b50e6c61908fc5d7b3642f1
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2020-09-11 19:10:31 -0300

    GolpeNormal PLayer

---
+++ SetembroJam/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private LayerMask platformLayerMask;
+    private GameManager gm;
+    private int damage;
+
+    private void Awake()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+    private void Update()
+    {
+        damage = gm.GetEnergiaVital();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null && (((1 << collision.gameObject.layer) & platformLayerMask) != 0))
+        {
+            EnemyHealth eh = collision.GetComponent<EnemyHealth>();
+            CheckPointHealth cph = collision.GetComponent<CheckPointHealth>();
+            if (eh != null)
+            {
+                eh.TakeDamage(damage);
+            }
+            if (cph != null)
+            {
+                cph.TakeDamage(damage);
+            }
+        }
+    }
+}

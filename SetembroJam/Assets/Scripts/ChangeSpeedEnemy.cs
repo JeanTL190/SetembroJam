5d6822ceccfca60496181baf17cbd64542787f97
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2020-09-11 00:41:13 -0300

    Enemy1

--- SetembroJam/Assets/Scripts/ChangeSpeedEnemy.cs
+++ SetembroJam/Assets/Scripts/ChangeSpeedEnemy.cs
@@ -4,27 +4,66 @@ using UnityEngine;
 
 public class ChangeSpeedEnemy : MonoBehaviour
 {
-    [SerializeField] float normalSpeed = 1f;
-    [SerializeField] float fastSpeed = 2f;
+    [SerializeField] private float normalSpeed = 1f;
+    [SerializeField] private float fastSpeed = 2f;
     [SerializeField] private LayerMask platformLayerMask;
+    [SerializeField] private float acel = 0.5f;
     private EnemyMoviment em;
+    private float speedAtual;
+    private bool inRange = false;
+    private bool atacando = false;
     private void Awake()
     {
+        speedAtual = normalSpeed;
         em = GetComponentInParent<EnemyMoviment>();
-        em.ChangeVel(normalSpeed);
+        em.ChangeVel(speedAtual);
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision != null && (((1 << collision.gameObject.layer) & platformLayerMask) != 0))
         {
-            em.ChangeVel(fastSpeed);
+            inRange = true;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision != null && (((1 << collision.gameObject.layer) & platformLayerMask) != 0))
         {
-            em.ChangeVel(normalSpeed);
+            inRange = false;
         }
+    }
+    public void SetAtaque(bool a)
+    {
+        atacando = a;
+    }
+    public bool GetInRange()
+    {
+        return inRange;
+    }
+    private void FixedUpdate()
+    {
+        if (!inRange)
+        {
+            if(speedAtual > normalSpeed)
+            {
+                speedAtual = Mathf.Clamp(speedAtual - acel, normalSpeed, fastSpeed);
+            }
+            else if (speedAtual<normalSpeed)
+            {
+                speedAtual = Mathf.Clamp(speedAtual + acel, 0, normalSpeed);
+            }
+        }
+        else
+        {
+            if (!atacando)
+            {
+                speedAtual = Mathf.Clamp(speedAtual + acel, 0, fastSpeed);
+            }
+            else
+            {
+                speedAtual = 0;
+            }
+        }
+        em.ChangeVel(speedAtual);
     }
 }
---
+++ SetembroJam/Assets/Scripts/HitBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    [SerializeField] private int dano = 1;
+    [SerializeField] private LayerMask platformLayerMask;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null && (((1 << collision.gameObject.layer) & platformLayerMask) != 0))
+        {
+            collision.GetComponent<PlayerHealth>().TakeDamage(dano);
+        }
+    }
+
+}
